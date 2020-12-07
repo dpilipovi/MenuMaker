96a6a6a9961966c1aa815e5997211774eb4a7470
dpilipovi <david.pilipovic9@gmail.com>
2020-12-07 18:27:50 +0100

    intial commit

---
+++ src/main/java/com/zapresic/menu_maker/MenuMakerApplication.java
@@ -0,0 +1,13 @@
+package com.zapresic.menu_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zapresic/menu_maker/MenuMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapresic.menu_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
